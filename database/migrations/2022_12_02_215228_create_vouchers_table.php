e58361bd3519ecec6593bbf6da26fbe91520283c
Chukwuemeka Okoroafor <emroking@yahoo.com>
2022-12-02 23:16:59 +0100

    Added resource, models and migrations

---
+++ database/migrations/2022_12_02_215228_create_vouchers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('voucher_code', 30)->unique();
+            $table->decimal('discount_amount', 10);
+            $table->integer('times_used')->default(0);
+            $table->dateTime('from_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('to_date');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
